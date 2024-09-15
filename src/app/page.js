56f9028fb381112105c03e09fde1db972ca5443a
SuhasChoppala <choppalasuhas.02@gmail.com>
2024-09-15 20:57:14 +0530

    modified nav

--- src/app/page.js
+++ src/app/page.js
@@ -105,64 +105,90 @@ function TodoApp() {
 
         <div className="sm:container mx-auto mobiles:mx-0">
           <div className="flex items-center justify-between mobiles:justify-between mobiles:pr-5 sm:pr-5 mobiles:items-start">
-            <div className="flex flex-col mobiles:flex-col mobiles:mt-2 mobiles:gap-4 sm:flex-row justify-between px-5 pt-5 my-10 gap-20 items-center">
+            <div className="flex flex-col mobiles:flex-col mobiles:mt-2 mobiles:gap-4 sm:flex-row justify-between px-5 pt-5 items-center">
               <h1 className={`text-3xl ${bodyColor == 'black' ? 'text-neutral-400' : 'text-black font-semibold'} mobiles:text-[31px] text-left`}>My ToDo App</h1>
-              <div className="sm:flex flex-row gap-4 items-center">
+            </div>
+            <div className="flex items-center mobiles:gap-3 sm:gap-5">
+              <div className="flex items-center pt-5 mobiles:mt-3.5">
+                <div onClick={toggleDarkMode} className={`cursor-pointer flex items-center`}>
+                  {bodyColor === 'black' ?
+                    <Image src='/dark.png' alt="dark-mode" height={24} width={24} />
+                    : <Image src='/dark2.png' alt="dark-mode" height={24} width={24} />}
+                </div>
+              </div>
+              <div className="flex items-center pt-5 mobiles:mt-3.5">
+                <button onClick={() => setIsCreateModalVisible(true)}>
+                  {bodyColor === 'black' ? <Image
+                    src="/add.png"
+                    alt="addIcon"
+                    width={30}
+                    height={30}
+                  /> :
+                    <Image
+                      src="/add-black.png"
+                      alt="addIcon"
+                      width={30}
+                      height={30}
+                      style={{ filter: 'contrast(200%)' }}
+                    />
+                  }
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="w-full flex px-6 mobiles:gap-3 my-4 pt-3 md:gap-5 lg:w-96">
+            <div className="w-1/2">
+              <div className="relative w-full">
                 <input
                   ref={searchInput}
                   onChange={(searchInput) => searchTodos(searchInput.currentTarget.value)}
                   placeholder="Search task"
-                  className={`w-full ${bodyColor == 'black' ? 'bg-black border border-neutral-700 text-neutral-400 font-extralight' : 'bg-white border border-black text-black font-normal'} px-3 py-0.5 rounded mobiles:w-full mobiles:mt-3.5 mobiles:px-1.5 pb-2 py-1 h-8`}
+                  className={`w-full pr-8 ${bodyColor == 'black' ? 'bg-black border border-neutral-700 text-neutral-400 font-extralight' : 'bg-white border border-black text-black font-normal'} px-3 py-0.5 rounded mobiles:w-full mobiles:px-1.5 pb-1 py-1 h-8`}
                 />
-
-                <div className="relative w-full mobiles:mt-4">
-                  <select
-                    ref={sortTodosByFilter}
-                    onChange={(sortTodosByFilter) => sortTodos(sortTodosByFilter.currentTarget.value)}
-                    className={`w-full ${bodyColor == 'black' ? 'bg-black border border-neutral-700 text-neutral-400 font-extralight' : 'bg-white border border-black text-black font-normal'} px-3 py-0.5 rounded mobiles:w-full mobiles:px-1.5 h-8 font-extralight pr-10 appearance-none`}>
-                    <option value="" disabled selected hidden>Filter</option>
-                    <option className="hover:bg-red-500" value='all'>All</option>
-                    <option className="hover:bg-red-500" value='completed'>Completed</option>
-                    <option className="hover:bg-red-500" value='notCompleted'>Not Completed</option>
-                  </select>
-                  <span className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
-                    <svg className={`w-4 h-4 ${bodyColor == 'black' ? 'text-neutral-400' : 'text-black'}`} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path>
-                    </svg>
-                  </span>
-                </div>
-
-                <div onClick={toggleDarkMode} className={`cursor-pointer flex items-center w-80 justify-center text-center rounded-full ${bodyColor == 'black' ? 'border border-neutral-400 border-1' : 'border border-black border-1'} mobiles:w-28 py-1 px-1 mobiles:mt-4`}>
-                  <Image src='/dark.png' alt="dark-mode" height={20} width={20} className={`${bodyColor == 'black' ? '' : 'filter invert'}`} />
-                  <span className={`ml-2 text-sm ${bodyColor == 'black' ? 'text-neutral-400' : 'text-black'} mobiles:ml-1`}>{bodyColor == 'black' ? 'Light Mode' : 'Dark Mode'}</span>
-                </div>
+                <span className="absolute right-2 top-1/2 transform -translate-y-1/2 mobiles:mt-0.5 lg:mt-0.5">
+                  {bodyColor === 'black' ?
+                    <Image src='/search.png' width={16} height={16} alt="search-icon" />
+                    : <Image src='/search2.png' width={16} height={16} alt="search-icon" />
+                  }
+                </span>
               </div>
 
+
             </div>
-            <div className="flex items-center my-10 pt-5 mobiles:mt-3.5">
-              <button onClick={() => setIsCreateModalVisible(true)}>
-                {bodyColor === 'black' ? <Image
-                  src="/add.png"
-                  alt="addIcon"
-                  width={30}
-                  height={30}
-                /> :
-                  <Image
-                    src="/add-black.png"
-                    alt="addIcon"
-                    width={30}
-                    height={30}
-                    style={{ filter: 'contrast(200%)' }}
-                  />
-                }
-              </button>
+            <div className="w-1/2">
+              <div className="relative w-full">
+                <select
+                  ref={sortTodosByFilter}
+                  onChange={(sortTodosByFilter) => sortTodos(sortTodosByFilter.currentTarget.value)}
+                  className={`w-full ${bodyColor == 'black' ? 'bg-black border border-neutral-700 text-neutral-400 font-extralight' : 'bg-white border border-black text-black font-normal'} px-3 py-0.5 rounded mobiles:w-full mobiles:px-1.5 h-8 font-extralight pr-10 appearance-none`}>
+                  <option value="" disabled selected hidden>Filter</option>
+                  <option className="hover:bg-red-500" value='all'>All</option>
+                  <option className="hover:bg-red-500" value='completed'>Completed</option>
+                  <option className="hover:bg-red-500" value='notCompleted'>Not Completed</option>
+                </select>
+                <span className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+                  <svg className={`w-4 h-4 ${bodyColor == 'black' ? 'text-neutral-400' : 'text-black'}`} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path>
+                  </svg>
+                </span>
+              </div>
             </div>
           </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6 mobiles:pt-2">
-            {filteredTodos.map(filteredTodo => (
-              <Todo key={filteredTodo.id} filteredTodo={filteredTodo}></Todo>
-            ))}
-          </div>
+
+          {filteredTodos && filteredTodos.length > 0 ?
+
+            (<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6 mobiles:pt-2">
+              {filteredTodos.map(filteredTodo => (
+                <Todo key={filteredTodo.id} filteredTodo={filteredTodo}></Todo>
+              ))}
+            </div>) :
+
+            (<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6 mobiles:pt-2">
+              <div className="w-80 flex justify-center items-center xl:w-full">
+                <p className="text-neutral-400 text-center">Nothing here yet, start by adding your first task!</p>
+              </div>
+            </div>)}
+
         </div>
 
         {isCreateModalVisible && <CreateTodo />}
--- src/components/deleteTodo.js
+++ src/components/deleteTodo.js
@@ -10,12 +10,10 @@ function DeleteTodo(props) {
 
     const { filteredTodo, setIsDeleteModalVisible } = props;
     const { removeTodo } = useContext(TodoContext);
-    const context = useContext(TodoContext);
 
 
-    const confirmDelete = () => {
-        console.log(context);
-        removeTodo(filteredTodo.id);
+    const confirmDelete = (_todId) => {
+        removeTodo(_todId);
         setIsDeleteModalVisible(false);
     }
 
@@ -54,7 +52,7 @@ function DeleteTodo(props) {
                         <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                             <button
                                 type="button"
-                                onClick={confirmDelete}
+                                onClick={() => confirmDelete(filteredTodo.id)}
                                 className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
                             >
                                 Confirm Delete
